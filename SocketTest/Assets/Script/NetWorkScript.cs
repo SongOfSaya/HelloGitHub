893fbc4b15d9bc7c0ac92bb7901ed578b959a0a9
SongOfSaya <18668706967@163.com>
2014-11-24 11:43:05 +0800

    Revert "Revert "项目导入""
    This reverts commit c898aa14f904dc1ce25110388836d7fdcbf094ac.

---
+++ SocketTest/Assets/Script/NetWorkScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+
+public class NetWorkScript
+{
+    private static NetWorkScript _instance;
+    private Socket _socket;
+    //
+    private string ip = "127.0.0.1";
+    private int port = 10;
+    private byte[] buff;
+
+    public static NetWorkScript getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new NetWorkScript();
+        }
+        return _instance;
+    }
+
+    public void init()
+    {
+        try
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Connect(ip, port);
+            Debug.Log("服务器连接成功");
+            _socket.BeginReceive(buff, 0, 1024, SocketFlags.None)
+        }
+        catch 
+        {
+            Debug.Log("服务器连接失败");
+        }
+    }
+}
